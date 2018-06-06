c610cf3463e60fa3ee5ae47182ec8bf55c67c4e1
Joe Healy <joe-healy@idexx.com>
2018-06-06 17:13:06 -0400

    initial commit, write model

---
+++ src/trainer/model.py
@@ -0,0 +1,75 @@
+'''
+Created on Jun 6, 2018
+
+@author: jhealy
+
+implements a fully-connected keras sequential model for ecgs
+'''
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+import json
+import numpy as np
+
+LABEL_DICT = {0: 'normal', 1: 'abnormal'}
+
+def create_model(input_dim, n_classes, units_arr=[1024, 1024, 512, 512], lr=0.001):
+    '''
+    TODO: Unit Test
+    creates keras sequential model, compiles it and returns it
+    '''
+    
+    model = Sequential()
+    for units in units_arr:
+        model.add(Dense(units, input_dim=input_dim, activation='relu'))
+        
+        input_dim=units
+    
+    model.add(Dense(n_classes, activation='softmax'))
+    compile_model(model, lr)
+    return model
+
+def compile_model(model, lr, loss='binary_crossentropy'):
+    '''
+    TODO: Unit Test
+    Compiles the model with the Adam optimizer and specified learning rate and returns it
+    '''
+    
+    model.compile(loss=loss, optimizer=Adam(lr=lr), metrics=['accuracy'])
+    return model
+
+def generate_inputs(dataFile, window_size=600):
+    '''
+    TODO: Unit Test
+    generate input arrays
+    
+    datafile - location of json file for each signal, 
+        TODO: doc with json design
+        
+    returns tuple of two lists (data, labels)
+    '''
+    
+    X = []
+    Y = []
+    
+    with open(dataFile, 'r') as df:
+        data = df.read()
+        data = json.loads(data)
+        for obj in data:
+            obj = json.loads(obj)
+            signal = obj['signal']
+            if  signal < window_size:
+                continue
+            if signal > window_size:
+                signal = signal[:600]
+            signal = np.asarray(signal)    
+            label = int(obj['label'])
+            X.append(signal)
+            Y.append(label)
+    
+    if len(X) != len(Y):
+        raise ValueError('ERROR: mismatch in data and labels')
+    
+    return (X, Y)
+    
