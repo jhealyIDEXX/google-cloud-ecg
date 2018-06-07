c261aa5cc6b6d23a61334b60ff2809d1c4ce23f8
Joe Healy <joe-healy@idexx.com>
2018-06-07 10:24:01 -0400

    finish packaging training code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+REQUIRED_PACKAGES = ['Keras>=2.0.4', 'h5py>=2.7.0', 'numpy', 'json']
+
+setup(
+    name='ecg_trainer',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description='ECG Trainer'
+    )
