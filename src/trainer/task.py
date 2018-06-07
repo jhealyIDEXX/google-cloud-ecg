e1300ce053a22023e2a0610f3751672ae91494cb
Joe Healy <joe-healy@idexx.com>
2018-06-07 10:24:01 -0400

    finish packaging training code

---
+++ src/trainer/task.py
@@ -0,0 +1,46 @@
+'''
+Created on Jun 6, 2018
+
+@author: jhealy
+'''
+
+import model
+import numpy as np
+import os
+import errno
+from sklearn.model_selection import train_test_split
+from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
+from numpy.f2py.crackfortran import verbose
+
+def train_main(data, output_dir, tboard_dir, batch_size, n_epochs=200, window_size=600):
+    
+    X, Y = model.generate_inputs(data)
+    X = np.asarray(X)
+    X = model.normalize(X)
+    
+    x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, shuffle=True)
+    
+    model = model.create_model(input_dim=(window_size, ), n_classes=2)
+    
+    epoch_savename = '{epoch:02d}-model.hdf5'
+    epochs_dir = '{}/epochs'.format(output_dir)
+    try:
+        os.makedirs(epochs_dir)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise
+    
+    model_savename = 'best-model.hdf5'
+    
+    epochs = ModelCheckpoint('{}/{}'.format(epoch_savename, epochs_dir))
+    bestModel = ModelCheckpoint('{}/{}'.format(output_dir, model_savename), monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    tb = TensorBoard(log_dir=tboard_dir)
+    earlystop = EarlyStopping(monitor='acc', patience=20)
+    
+    callbacks = [epochs, bestModel, tb, earlystop]
+    model.fit(x_train, y_train, verbose=1, 
+              validation_data=(x_test, y_test), 
+              callbacks=callbacks, 
+              batch_size=batch_size, epochs=n_epochs)
+if __name__ == '__main__':
+    
