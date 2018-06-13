027b868101d5abc2c25a502abb94e6311437894f
Joe Healy <joe-healy@idexx.com>
2018-06-13 13:46:54 -0400

    fix tabs/spaces

--- trainer/model.py
+++ trainer/model.py
@@ -15,12 +15,12 @@ import numpy as np
 LABEL_DICT = {0: 'normal', 1: 'abnormal'}
 
 def create_model(input_shape, n_classes, lr=0.001, loss='binary_crossentropy'):
-    '''
-    TODO: Unit Test
-    creates keras sequential model, compiles it and returns it
-    '''
-    
-    model = Sequential()
+	'''
+	TODO: Unit Test
+	creates keras sequential model, compiles it and returns it
+	'''
+	
+	model = Sequential()
 	model.add(Dense(1024, activation='relu', input_shape=input_shape))
 	model.add(Dropout(0.2))
 
@@ -35,58 +35,58 @@ def create_model(input_shape, n_classes, lr=0.001, loss='binary_crossentropy'):
 
 	model.add(Dense(num_classes, activation='softmax'))
 	model = compile_model(model, lr, loss=loss)
-    
-    return model
+	
+	return model
 
 def compile_model(model, lr, loss='binary_crossentropy'):
-    '''
-    TODO: Unit Test
-    Compiles the model with the Adam optimizer and specified learning rate and returns it
-    '''
-    
-    model.compile(loss=loss, optimizer=Adam(lr=lr), metrics=['accuracy'])
-    return model
+	'''
+	TODO: Unit Test
+	Compiles the model with the Adam optimizer and specified learning rate and returns it
+	'''
+	
+	model.compile(loss=loss, optimizer=Adam(lr=lr), metrics=['accuracy'])
+	return model
 
 def normalize(data, mean=None, stdev=None):
-    if mean == None:
-        mean = np.mean(data, axis=0)
-    if stdev == None:
-        stdev = np.mean(data, axis=0)
-        
-    return (data - mean) / stdev
+	if mean == None:
+		mean = np.mean(data, axis=0)
+	if stdev == None:
+		stdev = np.mean(data, axis=0)
+		
+	return (data - mean) / stdev
 
 def generate_inputs(dataFile, channel_used=0, window_size=600):
-    '''
-    TODO: Unit Test
-    generate input arrays
-    
-    datafile - location of json file for each signal, 
-        TODO: doc with json design
-        
-    returns tuple of two lists (data, labels)
-    '''
-    
-    X = []
-    Y = []
-    
-    with open(dataFile, 'r') as df:
-        data = df.read()
-        data = json.loads(data)
-        for obj in data:
-            channels = obj['channels']
-            signal = channels[str(channel_used)]
-            signal_len = len(signal)
-            if  signal_len < window_size:
-                continue
-            if signal_len > window_size:
-                signal = signal[:600]
-            signal = np.asarray(signal)    
-            label = int(obj['label'])
-            X.append(signal)
-            Y.append(label)
-    
-    if len(X) != len(Y):
-        raise ValueError('ERROR: mismatch in data and labels')
-    
-    return (X, Y)
-    
+	'''
+	TODO: Unit Test
+	generate input arrays
+	
+	datafile - location of json file for each signal, 
+		TODO: doc with json design
+		
+	returns tuple of two lists (data, labels)
+	'''
+	
+	X = []
+	Y = []
+	
+	with open(dataFile, 'r') as df:
+		data = df.read()
+		data = json.loads(data)
+		for obj in data:
+			channels = obj['channels']
+			signal = channels[str(channel_used)]
+			signal_len = len(signal)
+			if  signal_len < window_size:
+				continue
+			if signal_len > window_size:
+				signal = signal[:600]
+			signal = np.asarray(signal)	
+			label = int(obj['label'])
+			X.append(signal)
+			Y.append(label)
+	
+	if len(X) != len(Y):
+		raise ValueError('ERROR: mismatch in data and labels')
+	
+	return (X, Y)
+	
