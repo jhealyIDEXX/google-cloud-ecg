4d121ba28e3f830fcc2ca5bccf481aafa894713e
Joe Healy <joe-healy@idexx.com>
2018-06-13 12:18:14 -0400

    restructure

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 REQUIRED_PACKAGES = ['Keras>=2.0.4', 'h5py>=2.7.0', 'numpy', 'json']
 
 setup(
-    name='ecg_trainer',
+    name='trainer',
     version='0.1',
     install_requires=REQUIRED_PACKAGES,
     packages=find_packages(),
