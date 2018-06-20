3459957ce6b400ade89427a7f7a7bbb526fe4e7b
Joe Healy <joe-healy@idexx.com>
2018-06-20 15:17:10 -0400

    convert to keras template

--- trainer/model.py
+++ trainer/model.py
@@ -1,23 +1,41 @@
-'''
-Created on Jun 6, 2018
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
-@author: jhealy
+"""Implements the Keras Sequential model."""
 
-implements a fully-connected keras sequential model for ecgs
-'''
+import keras
+from keras import backend as K
+from keras import layers, models
+from keras.utils import np_utils
+from keras.backend import relu, softmax
 
-from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.optimizers import Adam
-import json
-import numpy as np
-import sys
-import tensorflow as tf 
+#Python2/3 compatibility imports
+from six.moves.urllib import parse as urlparse
+from builtins import range
 
-LABEL_DICT = {0: 'normal', 1: 'abnormal'}
+import tensorflow as tf
+from tensorflow.python.saved_model import builder as saved_model_builder
+from tensorflow.python.saved_model import tag_constants, signature_constants
+from tensorflow.python.saved_model.signature_def_utils_impl import predict_signature_def
 
-def create_model(input_shape, n_classes, lr=0.001, loss='binary_crossentropy'):
-	'''
+def model_fn(input_shape, 
+	labels_dim,
+	learning_rate=0.001, 
+	loss='binary_crossentropy', 
+	n_classes=2):
+  '''
 	TODO: Unit Test
 	creates keras sequential model, compiles it and returns it
 	'''
@@ -37,28 +55,34 @@ def create_model(input_shape, n_classes, lr=0.001, loss='binary_crossentropy'):
 
 	model.add(Dense(n_classes, activation='softmax'))
 	model = compile_model(model, lr, loss=loss)
-	
-	return model
+  
+  return model
 
-def compile_model(model, lr, loss='binary_crossentropy'):
-	'''
-	TODO: Unit Test
-	Compiles the model with the Adam optimizer and specified learning rate and returns it
-	'''
-	
-	model.compile(loss=loss, optimizer=Adam(lr=lr), metrics=['accuracy'])
-	return model
+def compile_model(model, learning_rate):
+  model.compile(loss='categorical_crossentropy',
+                optimizer=keras.optimizers.Adam(lr=learning_rate),
+                metrics=['accuracy'])
+  return model
 
-def normalize(data, mean=None, stdev=None):
-	if mean == None:
-		mean = np.mean(data, axis=0)
-	if stdev == None:
-		stdev = np.mean(data, axis=0)
-		
-	return (data - mean) / stdev
+def to_savedmodel(model, export_path):
+  """Convert the Keras HDF5 model into TensorFlow SavedModel."""
 
-def generate_inputs(dataFile, channel_used=0, window_size=600):
-	'''
+  builder = saved_model_builder.SavedModelBuilder(export_path)
+
+  signature = predict_signature_def(inputs={'input': model.inputs[0]},
+                                    outputs={'income': model.outputs[0]})
+
+  with K.get_session() as sess:
+    builder.add_meta_graph_and_variables(
+        sess=sess,
+        tags=[tag_constants.SERVING],
+        signature_def_map={
+            signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: signature}
+    )
+    builder.save()
+
+def generator_input(input_file, chunk_size, batch_size=64):
+  '''
 	TODO: Unit Test
 	generate input arrays
 	
@@ -95,4 +119,3 @@ def generate_inputs(dataFile, channel_used=0, window_size=600):
 		raise ValueError('ERROR: mismatch in data and labels')
 	
 	return (X, Y)
-	
--- trainer/task.py
+++ trainer/task.py
@@ -1,88 +1,223 @@
-'''
-Created on Jun 6, 2018
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
 
-@author: jhealy
-'''
+"""This code implements a Feed forward neural network using Keras API."""
+
+import argparse
+import glob
+import json
+import os
 
 import trainer.model as model
-import numpy as np
-import os
-import errno
-from sklearn.model_selection import train_test_split
-from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping
-from numpy.f2py.crackfortran import verbose
-import argparse
 
-def train_main(data, output_dir, tboard_dir, batch_size, n_epochs=200, window_size=600):
-	gcs = False
-	X, Y = model.generate_inputs(data)
-	print('outputs generated')
-	X = np.asarray(X)
-	X = model.normalize(X)
-	
-	x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, shuffle=True)
-	
-	fc_model = model.create_model(input_shape=(window_size, ), n_classes=2)
-	
-	if output_dir.startswith('gs://'):
-		gcs = True
-		try:
-			os.makedirs(output_dir[5:])
-		except OSError as e:
-			if e.errno != errno.EEXIST:
-				raise
+from keras.callbacks import TensorBoard, Callback, ModelCheckpoint
+from keras.models import load_model
+from tensorflow.python.lib.io import file_io
 
-	if gcs:
-		tboard_dir = 'tensorboard/{}'.format(output_dir[5:])
-		print('tboard_dir: {}'.format(tboard_dir))
-		try:
-			os.makedirs(tboard_dir)
-		except OSError as e:
-			if e.errno != errno.EEXIST:
-				raise
-	
-	epoch_savename = '{epoch:02d}-fc_model.hdf5'
-	epochs_dir = '{}/epochs'.format(output_dir)
-	if gcs:
-		epochs_dir='local_checkpoints/{}/'.format(output_dir[5:])
-	try:
-		os.makedirs(epochs_dir)
-	except OSError as e:
-		if e.errno != errno.EEXIST:
-			raise
-	
-	model_savename = 'best-fc_model.hdf5'
-	
-	epochs = ModelCheckpoint('{}/{}'.format(epochs_dir, epoch_savename))
-	bestModel = ModelCheckpoint('{}/{}'.format(output_dir, model_savename), monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-	if gcs:
-		bestModel = ModelCheckpoint('{}/{}'.format(output_dir[5:], model_savename), monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-		
-	tb = TensorBoard(log_dir=tboard_dir)
-	earlystop = EarlyStopping(monitor='acc', patience=20)
-	
-	callbacks = [epochs, bestModel, tb, earlystop]
-	fc_model.fit(np.array(x_train), np.array(y_train), verbose=1, 
-			  validation_data=(np.array(x_test), np.array(y_test)), 
-			  callbacks=callbacks, 
-			  batch_size=batch_size, epochs=n_epochs)
+INPUT_SHAPE = (,600)
+CLASS_SIZE = 2
 
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--train-files',
-					  required=True,
-					  type=str,
-					  help='Training files', nargs='+')
-	parser.add_argument('--job-dir',
-						required=True,
-						type=str,
-						help='output_dir')
-	parser.add_argument('--batch-size',
-						type=int,
-						default=64,
-						help='batch size')
-	args = parser.parse_args()
-	train_data = str(args.train_files[0])
-	output_dir = str(args.job_dir)
-	train_main(train_data, output_dir, 'tensorboard/{}'.format(output_dir), args.batch_size)
+# CHUNK_SIZE specifies the number of lines
+# to read in case the file is very large
+CHUNK_SIZE = 5000
+FILE_PATH = 'checkpoint.{epoch:02d}.hdf5'
+ECG_MODEL = 'ecg.hdf5'
 
+class ContinuousEval(Callback):
+  """Continuous eval callback to evaluate the checkpoint once
+     every so many epochs.
+  """
+
+  def __init__(self,
+               eval_frequency,
+               eval_files,
+               learning_rate,
+               job_dir,
+               steps=1000):
+    self.eval_files = eval_files
+    self.eval_frequency = eval_frequency
+    self.learning_rate = learning_rate
+    self.job_dir = job_dir
+    self.steps = steps
+
+  def on_epoch_begin(self, epoch, logs={}):
+    if epoch > 0 and epoch % self.eval_frequency == 0:
+
+      # Unhappy hack to work around h5py not being able to write to GCS.
+      # Force snapshots and saves to local filesystem, then copy them over to GCS.
+      model_path_glob = 'checkpoint.*'
+      if not self.job_dir.startswith("gs://"):
+        model_path_glob = os.path.join(self.job_dir, model_path_glob)
+      checkpoints = glob.glob(model_path_glob)
+      if len(checkpoints) > 0:
+        checkpoints.sort()
+        ecg_model = load_model(checkpoints[-1])
+        ecg_model = model.compile_model(ecg_model, self.learning_rate)
+        loss, acc = ecg_model.evaluate_generator(
+            model.generator_input(self.eval_files, chunk_size=CHUNK_SIZE),
+            steps=self.steps)
+        print('\nEvaluation epoch[{}] metrics[{:.2f}, {:.2f}] {}'.format(
+            epoch, loss, acc, ecg_model.metrics_names))
+        if self.job_dir.startswith("gs://"):
+          copy_file_to_gcs(self.job_dir, checkpoints[-1])
+      else:
+        print('\nEvaluation epoch[{}] (no checkpoints found)'.format(epoch))
+
+def dispatch(train_files,
+             eval_files,
+             job_dir,
+             train_steps,
+             eval_steps,
+             train_batch_size,
+             eval_batch_size,
+             learning_rate,
+             eval_frequency,
+             first_layer_size,
+             num_layers,
+             scale_factor,
+             eval_num_epochs,
+             num_epochs,
+             checkpoint_epochs):
+  ecg_model = model.model_fn(INPUT_SHAPE, CLASS_SIZE)
+
+  try:
+    os.makedirs(job_dir)
+  except:
+    pass
+
+  # Unhappy hack to work around h5py not being able to write to GCS.
+  # Force snapshots and saves to local filesystem, then copy them over to GCS.
+  checkpoint_path = FILE_PATH
+  if not job_dir.startswith("gs://"):
+    checkpoint_path = os.path.join(job_dir, checkpoint_path)
+
+  # Model checkpoint callback
+  checkpoint = ModelCheckpoint(
+      checkpoint_path,
+      monitor='val_loss',
+      verbose=1,
+      period=checkpoint_epochs,
+      mode='max')
+
+  # Continuous eval callback
+  evaluation = ContinuousEval(eval_frequency,
+                              eval_files,
+                              learning_rate,
+                              job_dir)
+
+  # Tensorboard logs callback
+  tblog = TensorBoard(
+      log_dir=os.path.join(job_dir, 'logs'),
+      histogram_freq=0,
+      write_graph=True,
+      embeddings_freq=0)
+
+  callbacks=[checkpoint, evaluation, tblog]
+
+  ecg_model.fit_generator(
+      model.generator_input(train_files),
+      steps_per_epoch=train_steps,
+      epochs=num_epochs,
+      callbacks=callbacks)
+
+  # Unhappy hack to work around h5py not being able to write to GCS.
+  # Force snapshots and saves to local filesystem, then copy them over to GCS.
+  if job_dir.startswith("gs://"):
+    ecg_model.save(CENSUS_MODEL)
+    copy_file_to_gcs(job_dir, ECG_MODEL)
+  else:
+    ecg_model.save(os.path.join(job_dir, ECG_MODEL))
+
+  # Convert the Keras model to TensorFlow SavedModel
+  model.to_savedmodel(ecg_model, os.path.join(job_dir, 'export'))
+
+# h5py workaround: copy local models over to GCS if the job_dir is GCS.
+def copy_file_to_gcs(job_dir, file_path):
+  with file_io.FileIO(file_path, mode='rb') as input_f:
+    with file_io.FileIO(os.path.join(job_dir, file_path), mode='w+') as output_f:
+        output_f.write(input_f.read())
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--train-files',
+                      required=True,
+                      type=str,
+                      help='Training files local or GCS', nargs='+')
+  parser.add_argument('--eval-files',
+                      required=True,
+                      type=str,
+                      help='Evaluation files local or GCS', nargs='+')
+  parser.add_argument('--job-dir',
+                      required=True,
+                      type=str,
+                      help='GCS or local dir to write checkpoints and export model')
+  parser.add_argument('--train-steps',
+                      type=int,
+                      default=100,
+                      help="""\
+                       Maximum number of training steps to perform
+                       Training steps are in the units of training-batch-size.
+                       So if train-steps is 500 and train-batch-size if 100 then
+                       at most 500 * 100 training instances will be used to train.
+                      """)
+  parser.add_argument('--eval-steps',
+                      help='Number of steps to run evalution for at each checkpoint',
+                      default=100,
+                      type=int)
+  parser.add_argument('--train-batch-size',
+                      type=int,
+                      default=40,
+                      help='Batch size for training steps')
+  parser.add_argument('--eval-batch-size',
+                      type=int,
+                      default=40,
+                      help='Batch size for evaluation steps')
+  parser.add_argument('--learning-rate',
+                      type=float,
+                      default=0.003,
+                      help='Learning rate for SGD')
+  parser.add_argument('--eval-frequency',
+                      default=10,
+                      help='Perform one evaluation per n epochs')
+  parser.add_argument('--first-layer-size',
+                     type=int,
+                     default=256,
+                     help='Number of nodes in the first layer of DNN')
+  parser.add_argument('--num-layers',
+                     type=int,
+                     default=2,
+                     help='Number of layers in DNN')
+  parser.add_argument('--scale-factor',
+                     type=float,
+                     default=0.25,
+                     help="""\
+                      Rate of decay size of layer for Deep Neural Net.
+                      max(2, int(first_layer_size * scale_factor**i)) \
+                      """)
+  parser.add_argument('--eval-num-epochs',
+                     type=int,
+                     default=1,
+                     help='Number of epochs during evaluation')
+  parser.add_argument('--num-epochs',
+                      type=int,
+                      default=20,
+                      help='Maximum number of epochs on which to train')
+  parser.add_argument('--checkpoint-epochs',
+                      type=int,
+                      default=5,
+                      help='Checkpoint per n training epochs')
+  parse_args, unknown = parser.parse_known_args()
+
+  dispatch(**parse_args.__dict__)
